ec537cb3fcf3debb124bde3712a623eba97185d0
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-07-10 01:14:52 +0530

    hacker news latest function added

--- app.js
+++ app.js
@@ -17,4 +17,45 @@ app.use(bodyParser.urlencoded({
 app.use(compression());
 app.use(helmet());
 
-module.exports = app;
+let newestHackerNews = [];
+
+
+const getNewestHackerNews = async () => {
+    newestHackerNews = [];
+    try {
+        let { data } = await axios.get("https://hnrss.org/newest.jsonfeed", {
+            responseType: 'json'
+        });
+
+        for (let news of data) {
+            news.date_published = new Date(news.date_published).toLocaleString('en-GB', {
+                timeZoneName: 'short',
+                hc: 'h24',
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+        }
+        newestHackerNews = Array.from(data);
+    } catch (err) {
+        debug(err);
+        logger(err);
+    }
+
+}
+
+
+getNewestHackerNews();
+
+
+
+
+
+module.exports = {
+    app,
+    getNewestHackerNews,
+
+};
